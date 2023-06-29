121b5a0179c152330fbde350b6a86134c3656018
Ron Tamerin <ron@tamerin.tech>
2023-06-29 11:38:30 +0200

    fix critical issue with gpt/listen

--- chat_gpt_service.py
+++ chat_gpt_service.py
@@ -11,12 +11,15 @@ openai.api_key = config["openai_key"]
 if "openai_org" in config:
     openai.organization = config["openai_org"]
 
+
 class ChatGPTService:
     def __init__(self, prompt="You are a helpful assistant."):
         self.history = [{"role": "system", "content": prompt}]
 
     def send_to_chat_gpt(self, message):
         self.history.append({"role": "user", "content": message})
-        response = openai.ChatCompletion.create(model="gpt-3.5-turbo",            messages=self.history            )
-        self.history.append({"role": "assistant", "content": response})
-        return str.strip(response['choices'][0]['message']['content'])
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=self.history
+        )
+        self.history.append({"role": "assistant", "content": response["choices"][0]["message"]["content"]})
+        return str.strip(response["choices"][0]["message"]["content"])
--- input_listener.py
+++ input_listener.py
@@ -6,6 +6,7 @@ import wave
 import uuid
 import os
 
+
 class InputListener:
     def __init__(self, silence_threshold=75, silence_duration=1.5):
         self.chunk = 1024
@@ -18,11 +19,13 @@ class InputListener:
         self.frames = []
 
     def start(self):
-        self.stream = self.audio.open(format=self.format,
-                                      channels=self.channels,
-                                      rate=self.rate,
-                                      input=True,
-                                      frames_per_buffer=self.chunk)
+        self.stream = self.audio.open(
+            format=self.format,
+            channels=self.channels,
+            rate=self.rate,
+            input=True,
+            frames_per_buffer=self.chunk,
+        )
 
     def stop(self):
         self.stream.stop_stream()
@@ -31,11 +34,15 @@ class InputListener:
 
     def save_audio_to_file(self, audio_data):
         # Generate a random file name
-        file_name = str(uuid.uuid4()) + '.wav'
-        file_path = os.path.join('', file_name) #/path/to/save/directory', file_name)
+        file_name = str(uuid.uuid4()) + ".wav"
+        file_path = os.path.join("", file_name)  # /path/to/save/directory', file_name)
+
+        # if the file already exists, delete it
+        if os.path.exists(file_path):
+            os.remove(file_path)
 
         # Save the audio data to the file
-        wf = wave.open(file_path, 'wb')
+        wf = wave.open(file_path, "wb")
         wf.setnchannels(self.channels)
         wf.setsampwidth(self.audio.get_sample_size(self.format))
         wf.setframerate(self.rate)
@@ -64,22 +71,30 @@ class InputListener:
         self.stop()
 
         # Save the audio data to a file and return the file path
-        audio_data = b''.join(self.frames)
+        audio_data = b"".join(self.frames)
         file_path = self.save_audio_to_file(audio_data)
+
+        # Clear out self.frames
+        self.frames = []
+
         return file_path
 
-
     def transcribe(self, audio_data):
-        client = boto3.client('transcribe')
+        client = boto3.client("transcribe")
         response = client.start_transcription_job(
-            TranscriptionJobName='MyTranscriptionJob',
-            Media={'MediaFileUri': audio_data},
-            MediaFormat='wav',
-            LanguageCode='en-US'
+            TranscriptionJobName="MyTranscriptionJob",
+            Media={"MediaFileUri": audio_data},
+            MediaFormat="wav",
+            LanguageCode="en-US",
         )
         while True:
-            status = client.get_transcription_job(TranscriptionJobName='MyTranscriptionJob')
-            if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
+            status = client.get_transcription_job(
+                TranscriptionJobName="MyTranscriptionJob"
+            )
+            if status["TranscriptionJob"]["TranscriptionJobStatus"] in [
+                "COMPLETED",
+                "FAILED",
+            ]:
                 break
             print("Not ready yet...")
             time.sleep(5)
