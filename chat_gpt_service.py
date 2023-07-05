30bc3a3359e7b5efe202beb1702d8a2725820c29
Ron Tamerin <ron@tamerin.tech>
2023-07-05 11:48:24 +0200

    limit response to 16k

--- chat_gpt_service.py
+++ chat_gpt_service.py
@@ -16,10 +16,26 @@ class ChatGPTService:
     def __init__(self, prompt="You are a helpful assistant."):
         self.history = [{"role": "system", "content": prompt}]
 
+    def _get_history_token_size(self):
+        tokens = 0
+        for x in self.history:
+            tokens += len(x["content"].split(" "))
+        return tokens
+
     def send_to_chat_gpt(self, message):
         self.history.append({"role": "user", "content": message})
+
+        #check if the total tokens length exceed the total of 16k tokens
+        while self._get_history_token_size() > 16000:
+            print("ChatGPT History too long, removing oldest message")
+            #remove the oldest message - except the first one (system role)
+            if len(self.history) > 2:
+                self.history.pop(1)
+            else:
+                break
+
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=self.history
+            model="gpt-3.5-turbo-16k", messages=self.history
         )
         self.history.append({"role": "assistant", "content": response["choices"][0]["message"]["content"]})
         return str.strip(response["choices"][0]["message"]["content"])
